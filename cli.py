b32432798d1635597decd66fce359ecd43acd117
s.ovchinnikov <kazatca@gmail.com>
2019-12-30 07:20:24 +1000

    fixed usage

--- cli.py
+++ cli.py
@@ -2,16 +2,23 @@ from getpass import getpass
 from seedstorage import SeedStorage
 from code import getCode
 
+def usage():
+    print("totp <command> <name>")
+    print("commands:")
+    print("  add <name>: add new seed")
+    print("  get <name>: print the code")
+
 def main(argv):
     if len(argv) < 3:
-        print("totp <command> <name>")
-        print("commands:")
-        print("  add <name>: add new seed")
-        print("  get <name>: print the code")
+        usage()
         exit()
 
     [ _, cmd, name ] = argv[:3]
 
+    if cmd not in ['add', 'get']:
+        usage()
+        exit()
+    
     storage = SeedStorage(name, getpass())
 
     if cmd == 'get':
